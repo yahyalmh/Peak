fc6227513b9d13f291f5b8f9d2ad9759bdfd3eef
yahyalmh <yahyamh01@gmail.com>
2022-09-17 14:46:55 +0200

    Add presentation directory

--- app/src/main/java/com/example/peak/presentation/MainActivity.kt
+++ app/src/main/java/com/example/peak/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.peak
+package com.example.peak.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.peak.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
