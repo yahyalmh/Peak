c4365c3c80ed94109247e44d4ea9e258ddfda774
yahyalmh <yahyamh01@gmail.com>
2022-09-17 13:36:11 +0200

    Add hilt application class

---
+++ app/src/main/java/com/example/peak/ApplicationLauncher.kt
@@ -0,0 +1,12 @@
+package com.example.peak
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author yaya (@yahyalmh)
+ * @since 17th September 2022
+ */
+
+@HiltAndroidApp
+class ApplicationLauncher : Application()
